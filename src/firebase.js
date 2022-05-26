84ba74fdf19d560b2cc00adea69bffef359c263b
Wayne <83718013+greagori@users.noreply.github.com>
2022-05-26 15:46:41 -0400

    Keys removed

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyBJEsGU9VE1gc08IkcQTgoSloriuW6OeSo",
+	apiKey: "***************************************",
 	authDomain: "robinhood-app-4118d.firebaseapp.com",
 	projectId: "robinhood-app-4118d",
 	storageBucket: "robinhood-app-4118d.appspot.com",
-	messagingSenderId: "980217875269",
-	appId: "1:980217875269:web:fa6cd9897eeb43ea11cc2c",
-	measurementId: "G-R3WH0L0NR7",
+	messagingSenderId: "***************************************",
+	appId: "***************************************",
+	measurementId: "***************************************",
 };
 
 // const firebaseApp = firebase.initializeApp(firebaseConfig);
